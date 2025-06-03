98fe7e2a7a6d72b35032005e6cd3ffe8fbe7f6f4
John T. Wodder II <git@varonathe.org>
2025-06-03 11:35:43 -0400

    Simplify error handling in example

--- examples/list-repos/src/main.rs
+++ examples/list-repos/src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use clap::Parser;
 use minigh::{Client, StatusError};
 use serde::{Deserialize, Serialize};
@@ -24,70 +25,60 @@ struct Arguments {
     owner: String,
 }
 
-fn main() -> ExitCode {
-    let args = Arguments::parse();
-    let token = match gh_token::get() {
-        Ok(token) => token,
-        Err(e) => {
-            eprintln!("Failed to fetch GitHub token: {e}");
-            return ExitCode::FAILURE;
-        }
-    };
-    let client = match Client::new(&token) {
-        Ok(client) => client,
-        Err(e) => {
-            eprintln!("{e}");
-            return ExitCode::FAILURE;
-        }
-    };
-    let mut first = true;
-    match client.paginate::<Repository>(&format!("/users/{}/repos", args.owner)) {
-        Ok(repos) => {
-            for repo in repos {
-                if args.json {
-                    println!(
-                        "{}",
-                        serde_json::to_string_pretty(&repo)
-                            .expect("serializing Repository should not fail")
-                    );
-                } else {
-                    if !std::mem::replace(&mut first, false) {
-                        println!();
-                    }
-                    println!("Repository: {}", repo.full_name);
-
-                    println!("URL: {}", repo.html_url);
-
-                    println!(
-                        "Description: {}",
-                        repo.description.as_deref().unwrap_or("-")
-                    );
-
-                    println!("Language: {}", repo.language.as_deref().unwrap_or("-"));
-
-                    print!("Homepage: ");
-                    if let Some(hp) = repo.homepage.as_ref().filter(|hp| !hp.is_empty()) {
-                        println!("{hp}");
-                    } else {
-                        println!("-");
-                    }
-
-                    print!("Topics: ");
-                    if repo.topics.is_empty() {
-                        println!("-");
-                    } else {
-                        println!("{}", repo.topics.join(", "));
-                    }
-
-                    println!("Stars: {}", repo.stargazers_count);
-                    println!("Forks: {}", repo.forks_count);
+impl Arguments {
+    fn run(&self) -> anyhow::Result<()> {
+        let token = gh_token::get().context("Failed to fetch GitHub token")?;
+        let client = Client::new(&token)?;
+        let mut first = true;
+        let repos = client.paginate::<Repository>(&format!("/users/{}/repos", self.owner))?;
+        for repo in repos {
+            if self.json {
+                println!(
+                    "{}",
+                    serde_json::to_string_pretty(&repo)
+                        .expect("serializing Repository should not fail")
+                );
+            } else {
+                if !std::mem::replace(&mut first, false) {
+                    println!();
                 }
+                println!("Repository: {}", repo.full_name);
+
+                println!("URL: {}", repo.html_url);
+
+                println!(
+                    "Description: {}",
+                    repo.description.as_deref().unwrap_or("-")
+                );
+
+                println!("Language: {}", repo.language.as_deref().unwrap_or("-"));
+
+                print!("Homepage: ");
+                if let Some(hp) = repo.homepage.as_ref().filter(|hp| !hp.is_empty()) {
+                    println!("{hp}");
+                } else {
+                    println!("-");
+                }
+
+                print!("Topics: ");
+                if repo.topics.is_empty() {
+                    println!("-");
+                } else {
+                    println!("{}", repo.topics.join(", "));
+                }
+
+                println!("Stars: {}", repo.stargazers_count);
+                println!("Forks: {}", repo.forks_count);
             }
-            ExitCode::SUCCESS
         }
+        Ok(())
+    }
+}
+
+fn main() -> ExitCode {
+    match Arguments::parse().run() {
+        Ok(()) => ExitCode::SUCCESS,
         Err(e) => {
-            // Use anyhow to display the error chain
-            let e = anyhow::Error::new(e);
             eprintln!("{e:?}");
             if let Some(body) = e
                 .downcast_ref::<StatusError>()
